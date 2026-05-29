4888cb07cc4ae6c30e123b3ac129dde40e4478bd
DataDarrell <darrell.e.loutner@bmwmc.com>
2026-05-29 11:10:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,5 +53,9 @@ if ingredients_List:
         
         st.success('Your Smoothie is ordered!', icon="✅")
 
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
+
        
     
