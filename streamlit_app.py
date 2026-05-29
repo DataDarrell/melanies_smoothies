7d260d652700538b6441f8d02e24fe4159d2bd05
DataDarrell <darrell.e.loutner@bmwmc.com>
2026-05-29 10:35:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+from snowflake.snowpark.functions import col
 #from snowflake.snowpark.context import get_active_session
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie:cup_with_straw:")
@@ -13,8 +14,7 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your Smoothie will be:", name_on_order)
 
-#import Column
-from snowflake.snowpark.functions import col
+
 
 #session = get_active_session()
 cnx = st.connection("Snowflake")
